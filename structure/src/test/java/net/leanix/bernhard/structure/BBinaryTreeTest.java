464062e8a2c9dd92de02b8a38bb3eb80e2c94f54
Bernhard Landsberg <bernhard.landsberg@leanix.net>
2017-07-25 11:35:48 +0200

    Complex tree

--- structure/src/test/java/net/leanix/bernhard/structure/BBinaryTreeTest.java
+++ structure/src/test/java/net/leanix/bernhard/structure/BBinaryTreeTest.java
@@ -90,6 +90,35 @@ public class BBinaryTreeTest {
         assertThat(result).containsExactly("Root", "Root/Left", "Root/Right", "Root/Left/Left");
     }
 
+    @Test
+    public void breadthFirst_complexTree_shouldReturnCorrectTraversal() {
+        // given
+        BBinaryTree.Node root = new BBinaryTree.Node("Root");
+        b.setRoot(root);
+        BBinaryTree.Node left = new BBinaryTree.Node("Root/Left");
+        BBinaryTree.Node leftleft = new BBinaryTree.Node("Root/Left/Left");
+        BBinaryTree.Node leftright = new BBinaryTree.Node("Root/Left/Right");
+        BBinaryTree.Node right = new BBinaryTree.Node("Root/Right");
+        BBinaryTree.Node rightleft = new BBinaryTree.Node("Root/Right/Left");
+        BBinaryTree.Node rightleftleft = new BBinaryTree.Node("Root/Right/Left/Left");
+        BBinaryTree.Node rightleftright = new BBinaryTree.Node("Root/Right/Left/Right");
+        root.setLeft(left);
+        left.setLeft(leftleft);
+        left.setRight(leftright);
+        root.setRight(right);
+        right.setLeft(rightleft);
+        rightleft.setLeft(rightleftleft);
+        rightleft.setRight(rightleftright);
+        rightleftleft.setLeft(new BBinaryTree.Node("Root/Right/Left/Left/Left"));
+        rightleftright.setRight(new BBinaryTree.Node("Root/Right/Left/Right/Right"));
+
+        // when
+        List<String> result = b.breadthFirst();
+
+        // then
+        assertThat(result).containsExactly("Root", "Root/Left", "Root/Right", "Root/Left/Left", "Root/Left/Right", "Root/Right/Left", "Root/Right/Left/Left", "Root/Right/Left/Right", "Root/Right/Left/Left/Left", "Root/Right/Left/Right/Right");
+    }
+
     /*
      ################ depth-first ##############
     */
@@ -162,5 +191,34 @@ public class BBinaryTreeTest {
 
         // then
         assertThat(result).containsExactly("Root", "Root/Left", "Root/Left/Left", "Root/Right");
+    }
+
+    @Test
+    public void depthFirst_complexTree_shouldReturnCorrectTraversal() {
+        // given
+        BBinaryTree.Node root = new BBinaryTree.Node("Root");
+        b.setRoot(root);
+        BBinaryTree.Node left = new BBinaryTree.Node("Root/Left");
+        BBinaryTree.Node leftleft = new BBinaryTree.Node("Root/Left/Left");
+        BBinaryTree.Node leftright = new BBinaryTree.Node("Root/Left/Right");
+        BBinaryTree.Node right = new BBinaryTree.Node("Root/Right");
+        BBinaryTree.Node rightleft = new BBinaryTree.Node("Root/Right/Left");
+        BBinaryTree.Node rightleftleft = new BBinaryTree.Node("Root/Right/Left/Left");
+        BBinaryTree.Node rightleftright = new BBinaryTree.Node("Root/Right/Left/Right");
+        root.setLeft(left);
+        left.setLeft(leftleft);
+        left.setRight(leftright);
+        root.setRight(right);
+        right.setLeft(rightleft);
+        rightleft.setLeft(rightleftleft);
+        rightleft.setRight(rightleftright);
+        rightleftleft.setLeft(new BBinaryTree.Node("Root/Right/Left/Left/Left"));
+        rightleftright.setRight(new BBinaryTree.Node("Root/Right/Left/Right/Right"));
+
+        // when
+        List<String> result = b.depthFirst();
+
+        // then
+        assertThat(result).containsExactly("Root", "Root/Left", "Root/Left/Left", "Root/Left/Right", "Root/Right", "Root/Right/Left", "Root/Right/Left/Left", "Root/Right/Left/Left/Left", "Root/Right/Left/Right", "Root/Right/Left/Right/Right");
     }
 }
