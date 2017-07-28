5f440292e73c3cb52d387250b1f393e807db4557
Bernhard Landsberg <bernhard.landsberg@leanix.net>
2017-07-28 15:47:03 +0200

    Handling for negative numbers added

--- structure/src/main/java/net/leanix/bernhard/misc/Recursion.java
+++ structure/src/main/java/net/leanix/bernhard/misc/Recursion.java
@@ -1,5 +1,7 @@
 package net.leanix.bernhard.misc;
 
+import jdk.nashorn.internal.ir.IfNode;
+
 public class Recursion {
 
     public int sum(int a, int b) {
@@ -9,7 +11,28 @@ public class Recursion {
         if ( a == 0 ) {
             return b;
         }
+        if (b > a) {
+            int temp = a;
+            b = a;
+            a = temp;
 
+        }
+        boolean negativecheck = false;
+        // both numbers negative
+        if (a < 0 && b < 0) {
+            a = Math.abs(a);
+            b = Math.abs(b);
+            negativecheck = true;
+        }
+        //only one number negative
+
+        if (b < 0) {
+            return sum(a, b + 1) - 1;
+        }
+
+        if (negativecheck) {
+            return (sum(a, b - 1) + 1) * (- 1);
+        }
         return sum(a, b - 1) + 1;
     }
 
@@ -23,15 +46,62 @@ public class Recursion {
         if (a == 0 || b == 0) {
             return 0;
         }
+        // both numbers negative
+        if (a < 0 && b < 0) {
+            a = Math.abs(a);
+            b = Math.abs(b);
+        }
+        //only one number negative
+        boolean negativecheck = false;
+        if (a < 0 || b < 0) {
+            a = Math.abs(a);
+            b = Math.abs(b);
+            negativecheck = true;
+        }
+
+        if (negativecheck) {
+                return (prod(a, b - 1) + a) * (- 1);
+        }
 
         return prod(a, b - 1) + a;
     }
 
     public int pow(int a, int b) {
-        return 0;
+        if ( b == 0 ) {
+            return 1;
+        }
+        if ( a == 1 ) {
+            return a;
+        }
+        if (a == 0) {
+            return 0;
+        }
+
+        if (b < 0) {
+            return a;
+        }
+
+        boolean negativecheck = false;
+        if (a < 0) {
+            a = Math.abs(a);
+            negativecheck = true;
+        }
+
+        if (negativecheck) {
+            return (pow(a, b -1) * a) * (- 1);
+        }
+        return pow(a, b -1) * a;
     }
 
     public int fac(int a) {
-        return 0;
+        if (a < 0) {
+            return a;
+        }
+
+        if ( a == 1 || a == 0 ) {
+            return a;
+        }
+
+        return fac(a - 1) * a;
     }
 }
--- structure/src/test/java/net/leanix/bernhard/misc/RecursionTest.java
+++ structure/src/test/java/net/leanix/bernhard/misc/RecursionTest.java
@@ -79,6 +79,46 @@ public class RecursionTest {
     }
 
     @Test
+    public void sum_minus4And2() {
+        // given
+        int a = -4;
+        int b = 2;
+
+        // when
+        int s = r.sum(a, b);
+
+        // then
+        assertThat(s).isEqualTo(-2);
+    }
+
+    @Test
+    public void sum_1AndMinus4() {
+        // given
+        int a = 1;
+        int b = -4;
+
+        // when
+        int s = r.sum(a, b);
+
+        // then
+        assertThat(s).isEqualTo(-3);
+    }
+
+    @Test
+    public void sum_Minus1AndMinus2() {
+        // given
+        int a = -1;
+        int b = -2;
+
+        // when
+        int s = r.sum(a, b);
+
+        // then
+        assertThat(s).isEqualTo(-3);
+    }
+
+
+    @Test
     public void times_0And0() {
         // given
         int a = 0;
@@ -169,6 +209,45 @@ public class RecursionTest {
         assertThat(p).isEqualTo(42);
     }
 
+    @Test
+    public void times_Minus6And7() {
+        // given
+        int a = -6;
+        int b = 7;
+
+        // when
+        int p = r.prod(a, b);
+
+        // then
+        assertThat(p).isEqualTo(-42);
+    }
+
+    @Test
+    public void times_Minus6AndMinus7() {
+        // given
+        int a = -6;
+        int b = -7;
+
+        // when
+        int p = r.prod(a, b);
+
+        // then
+        assertThat(p).isEqualTo(42);
+    }
+
+    @Test
+    public void times_6AndMinus7() {
+        // given
+        int a = 6;
+        int b = -7;
+
+        // when
+        int p = r.prod(a, b);
+
+        // then
+        assertThat(p).isEqualTo(-42);
+    }
+
     // TODO: add other reasonable tests
 
     @Test
@@ -184,6 +263,33 @@ public class RecursionTest {
         assertThat(p).isEqualTo(256);
     }
 
+    @Test
+    public void pow_Minus2And8() {
+        // given
+        int a = -2;
+        int b = 8;
+
+        // when
+        int p = r.pow(a, b);
+
+        // then
+        assertThat(p).isEqualTo(-256);
+    }
+
+    @Test
+    public void pow_2AndMinus8_ShouldDoNothing() {
+        // given
+        int a = 2;
+        int b = -8;
+
+        // when
+        int p = r.pow(a, b);
+
+        // then
+        assertThat(p).isEqualTo(a);
+    }
+
+
     // TODO: add other reasonable tests
 
     @Test
@@ -196,5 +302,29 @@ public class RecursionTest {
 
         // then
         assertThat(f).isEqualTo(3628800);
+    }
+
+    @Test
+    public void fac_Minus10_ShouldDoNothing() {
+        // given
+        int a = -10;
+
+        // when
+        int f = r.fac(a);
+
+        // then
+        assertThat(f).isEqualTo(- 10);
+    }
+
+    @Test
+    public void fac_Minus0() {
+        // given
+        int a = 0;
+
+        // when
+        int f = r.fac(a);
+
+        // then
+        assertThat(f).isEqualTo(0);
     }
 }
